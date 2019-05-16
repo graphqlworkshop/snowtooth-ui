bcda097d278cf4072d6356bc2cc2c371c1bb22a8
MoonTahoe <alex@moonhighway.com>
2019-05-16 11:37:56 -0500

    II. ApolloClient configuration

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './App'
-import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from 'react-apollo'
+import React from "react";
+import { render } from "react-dom";
+import App from "./App";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { createHttpLink } from "apollo-link-http";
+import { ApolloProvider } from "@apollo/react-hooks";
 
-const client = new ApolloClient({
-  uri: 'http://localhost:4000'
-})
+const link = createHttpLink({ uri: "http://localhost:4000" });
+const cache = new InMemoryCache();
+const client = new ApolloClient({ link, cache });
 
 render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
