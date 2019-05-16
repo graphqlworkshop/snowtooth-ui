f9b96a1568668cf1efaeaab7793bab14d4338065
MoonTahoe <alex@moonhighway.com>
2019-05-16 12:25:41 -0500

    useQuery to load allLifts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react'
-import { gql } from 'apollo-boost'
-import { Query, Mutation } from 'react-apollo'
-import styled from 'styled-components'
+import React from "react";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
 
 const ALL_LIFTS_QUERY = gql`
   query {
@@ -16,88 +15,32 @@ const ALL_LIFTS_QUERY = gql`
       }
     }
   }
-`
+`;
 
-const LIFT_STATUS_MUTATION = gql`
-  mutation SetLiftStatus($id: ID!, $status: LiftStatus!) {
-    setLiftStatus(id: $id, status: $status) {
-      id
-      name
-      status
-    }
-  }
-`
-
-const Button = styled.div`
-    border-radius: 50%;
-    background-color: ${props => (props.selected ? props.color : 'none')};
-    border: ${props => (props.selected ? 'none' : `solid 2px ${props.color}`)}
-    width: 30px;
-    height: 30px;
-`
-
-const App = () => (
-  <Query query={ALL_LIFTS_QUERY}>
-    {({ loading, data, error }) => {
-      if (error) return `Error! ${error.message}`
-      if (loading) return 'Loading...'
-      return (
-        <section>
-          {!loading &&
-            data.allLifts.map(lift => (
-              <div key={lift.id}>
-                <h3>{lift.name}</h3>
-                <Mutation mutation={LIFT_STATUS_MUTATION}>
-                  {changeStatus => (
-                    <Button
-                      selected={lift.status === 'OPEN'}
-                      color="green"
-                      onClick={() =>
-                        changeStatus({
-                          variables: { id: lift.id, status: 'OPEN' }
-                        })
-                      }
-                    />
-                  )}
-                </Mutation>
-                <Mutation mutation={LIFT_STATUS_MUTATION}>
-                  {changeStatus => (
-                    <Button
-                      selected={lift.status === 'HOLD'}
-                      color="yellow"
-                      onClick={() =>
-                        changeStatus({
-                          variables: { id: lift.id, status: 'HOLD' }
-                        })
-                      }
-                    />
-                  )}
-                </Mutation>
-                <Mutation mutation={LIFT_STATUS_MUTATION}>
-                  {changeStatus => (
-                    <Button
-                      selected={lift.status === 'CLOSED'}
-                      color="red"
-                      onClick={() =>
-                        changeStatus({
-                          variables: { id: lift.id, status: 'CLOSED' }
-                        })
-                      }
-                    />
-                  )}
-                </Mutation>
-                <h3>Trails</h3>
-                <ul>
-                  {lift.trailAccess.map(trail => (
-                    <li key={trail.id}>{trail.name}</li>
-                  ))}
-                </ul>
-              </div>
+export default function App() {
+  const { loading, data } = useQuery(ALL_LIFTS_QUERY);
+  return (
+    <section>
+      <h1>Snowtooth Lift Status</h1>
+      {loading && <p>loading...</p>}
+      {data && !loading && (
+        <table className="lifts">
+          <thead>
+            <tr>
+              <th>Lift Name</th>
+              <th>Current Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.allLifts.map(lift => (
+              <tr>
+                <td>{lift.name}</td>
+                <td>{lift.status}</td>
+              </tr>
             ))}
-        </section>
-      )
-    }}
-  </Query>
-)
-
-export default App
+          </tbody>
+        </table>
+      )}
+    </section>
+  );
+}
